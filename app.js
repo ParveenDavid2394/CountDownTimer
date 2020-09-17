18f32f6a5120069aa1e571b615ac260e32d0567e
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-17 21:52:16 +0800

    set the deadline and display it to the html

--- app.js
+++ app.js
@@ -21,3 +21,22 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+
+// get target elements
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2020, 5, 23, 18, 30);
+
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const day = weekdays[futureDate.getDay()];
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const seconds = futureDate.getSeconds();
+
+giveaway.textContent = `Giveaway ends on ${day}, ${date} ${month} ${year},   ${hours}:${minutes}pm`;
+
